fda3347d8d68198c6ff89184a2224475d0b92de0
Qing Lan <47475974+qinglangg@users.noreply.github.com>
2019-11-26 22:42:12 +0800

    卿岚1.12.2OP完成，待执着修复1.12新加物品材质问题和1.7.10同步recipe

--- src/main/java/com/ore/oreplant/OrePlantRecipe.java
+++ src/main/java/com/ore/oreplant/OrePlantRecipe.java
@@ -23,10 +23,10 @@ public class OrePlantRecipe {
 		GameRegistry.addSmelting(Crops.reedGlowstone, new ItemStack(Items.GLOWSTONE_DUST, 2), 5.0f);
 		GameRegistry.addSmelting(Crops.reedGold, new ItemStack(Items.GOLD_INGOT, 1), 5.0f);
 		GameRegistry.addSmelting(Crops.reedIron, new ItemStack(Items.IRON_INGOT, 1), 5.0f);
-		GameRegistry.addSmelting(Crops.reedLapis, new ItemStack(Items.DYE, 2,4), 5.0f);
+		GameRegistry.addSmelting(Crops.reedLapis, new ItemStack(Items.DYE, 1,4), 5.0f);
 		GameRegistry.addSmelting(Crops.reedObsidian, new ItemStack(Blocks.OBSIDIAN, 1), 5.0f);
-		GameRegistry.addSmelting(Crops.reedQuartz, new ItemStack(Items.QUARTZ, 2), 5.0f);
-		GameRegistry.addSmelting(Crops.reedRedstone, new ItemStack(Items.REDSTONE, 2), 5.0f);
+		GameRegistry.addSmelting(Crops.reedQuartz, new ItemStack(Items.QUARTZ, 1), 5.0f);
+		GameRegistry.addSmelting(Crops.reedRedstone, new ItemStack(Items.REDSTONE, 1), 5.0f);
 		//树干
 		GameRegistry.addSmelting(Crops.woodCoal, new ItemStack(Items.COAL, 1,0), 5.0f);
 		GameRegistry.addSmelting(Crops.woodDiamond, new ItemStack(Items.DIAMOND, 1), 5.0f);
@@ -34,10 +34,10 @@ public class OrePlantRecipe {
 		GameRegistry.addSmelting(Crops.woodGlowstone, new ItemStack(Items.GLOWSTONE_DUST, 2), 5.0f);
 		GameRegistry.addSmelting(Crops.woodGold, new ItemStack(Items.GOLD_INGOT, 1), 5.0f);
 		GameRegistry.addSmelting(Crops.woodIron, new ItemStack(Items.IRON_INGOT, 1), 5.0f);
-		GameRegistry.addSmelting(Crops.woodLapis, new ItemStack(Items.DYE, 2,4), 5.0f);
-		GameRegistry.addSmelting(Crops.woodRedstone, new ItemStack(Items.REDSTONE, 2), 5.0f);
+		GameRegistry.addSmelting(Crops.woodLapis, new ItemStack(Items.DYE, 1,4), 5.0f);
+		GameRegistry.addSmelting(Crops.woodRedstone, new ItemStack(Items.REDSTONE, 1), 5.0f);
 		GameRegistry.addSmelting(Crops.woodObsidian, new ItemStack(Blocks.OBSIDIAN, 1), 5.0f);
-		GameRegistry.addSmelting(Crops.woodQuartz, new ItemStack(Items.QUARTZ, 2), 5.0f);
+		GameRegistry.addSmelting(Crops.woodQuartz, new ItemStack(Items.QUARTZ, 1), 5.0f);
 		//劣质钻石矿
 		GameRegistry.addSmelting(OreBlock.diamond_ore, new ItemStack(Items.DIAMOND, 1), 5.0f);
     }
